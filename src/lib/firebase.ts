5738b0d9a6af25b488bf2445ce1d5f9a6ee5c273
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-29 12:42:00 +0100

    Update dependencies and Firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,16 +4,22 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfHIK-s6oFyPW2bOJMkXV_Lsn6O-VYMcc",
-  authDomain: "sgq-next.firebaseapp.com",
-  projectId: "sgq-next",
-  storageBucket: "sgq-next.firebasestorage.app",
-  messagingSenderId: "68599267755",
-  appId: "1:68599267755:web:7cee598fc15383e67952ee"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
